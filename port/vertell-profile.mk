1ea32ddc1bf410692b144b3787e26dcf43e025be
maks <makssbaks@gmail.com>
2026-09-11 01:59:52 +0300

    Fix sysupgrade compatibility metadata

--- port/vertell-profile.mk
+++ port/vertell-profile.mk
@@ -1,11 +1,12 @@
 define Device/vertell_vt-mt7621d
   $(Device/nand)
+  DEVICE_COMPAT_VERSION := 1.0
+  DEVICE_COMPAT_MESSAGE :=
   DEVICE_VENDOR := Vertell
   DEVICE_MODEL := VT-STREET-M2
   IMAGE_SIZE := 121344k
   DEVICE_PACKAGES := -uboot-envtools \
 	kmod-usb3 kmod-usb-acm kmod-usb-net-cdc-ncm \
 	kmod-usb-net-qmi-wwan kmod-usb-serial-option uqmi
-  SUPPORTED_DEVICES += vertell,vt-mt7621d
 endef
 TARGET_DEVICES += vertell_vt-mt7621d
