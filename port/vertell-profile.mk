8c5e424296e0fcb8bfb1090305b7ff4a05fc2403
maks <makssbaks@gmail.com>
2026-09-12 13:24:49 +0300

    Drop unused Wi-Fi stack from VT-STREET-M2 image

--- port/vertell-profile.mk
+++ port/vertell-profile.mk
@@ -3,7 +3,7 @@ define Device/vertell_vt-mt7621d
   DEVICE_VENDOR := Vertell
   DEVICE_MODEL := VT-STREET-M2
   IMAGE_SIZE := 121344k
-  DEVICE_PACKAGES := -uboot-envtools \
+  DEVICE_PACKAGES := -uboot-envtools -wpad-basic-mbedtls -hostapd-common \
 	kmod-usb3 kmod-usb-acm kmod-usb-net-cdc-ncm \
 	kmod-usb-net-qmi-wwan kmod-usb-serial-option uqmi
 endef
